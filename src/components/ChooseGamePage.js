e40cde4803c593801a0f4aeafb10cc535a551fa2
Quddus Rahman <qrahman115@gmail.com>
2023-08-16 09:51:31 +0100

    add player button added to classic mode

--- src/components/ChooseGamePage.js
+++ src/components/ChooseGamePage.js
@@ -13,43 +13,49 @@ function ChooseGamePage() {
     setPlayers(newPlayers);
   }
 
+  const addAnotherPlayer = () => {
+    setPlayers([...players, '']);
+  };
+
   const startGame = () => {
     const playerData = players.map(name => ({ name, score: parseInt(game) }));
     console.log("Players to be passed:", playerData);
     navigate('/play', { state: { players: playerData } });
-  };
+};
 
-    return (
-        <div className="container mt-5">
-            <h2>Choose game:</h2>
+  return (
+    <div className="container mt-5">
+      <h2>Choose game:</h2>
 
-            <select className="form-select" value={game} onChange={(e) => setGame(e.target.value)}>
-                <option value="201">201</option>
-                <option value="501">501</option>
-            </select>
+      <select className="form-select" value={game} onChange={(e) => setGame(e.target.value)}>
+          <option value="201">201</option>
+          <option value="501">501</option>
+      </select>
 
-            <div className="mt-3">
-                {players.map((player, index) => (
-                    <div className="mb-2" key={index}>
-                        <input 
-                            className="form-control"
-                            placeholder={`Player ${index + 1} Name`} 
-                            value={player}
-                            onChange={(e) => handlePlayerChange(index, e)}
-                        />
-                    </div>
-                ))}
-            </div>
+      <div className="mt-3">
+          {players.map((player, index) => (
+              <div className="mb-2" key={index}>
+                  <input 
+                      className="form-control"
+                      placeholder={`Player ${index + 1} Name`} 
+                      value={player}
+                      onChange={(e) => handlePlayerChange(index, e)}
+                  />
+              </div>
+          ))}
+      </div>
 
-            <button 
-                className="btn btn-primary mt-3" 
-                disabled={players.some(p => !p.trim())} 
-                onClick={startGame}
-            >
-                Start Game
-            </button>
-        </div>
-    );
+      <button className="btn btn-secondary mt-3" onClick={addAnotherPlayer}>Add another player</button>
+
+      <button 
+            className="btn btn-primary mt-3"
+            onClick={startGame}
+            disabled={players.some(p => !p.trim())}
+        >
+            Start Game
+        </button>
+    </div>
+  );
 }
 
 export default ChooseGamePage;
