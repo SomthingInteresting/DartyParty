b4d6e71b458df0e913ac3b5ad2dee367bd248ee8
Quddus Rahman <qrahman115@gmail.com>
2023-08-16 10:57:32 +0100

    added dartboard svg and adjusted colours

---
+++ src/components/Dartboard.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import './Dartboard.css';
+
+function Dartboard() {
+
+  useEffect(() => {
+    var wide = 400;
+    for (var i = 0; i <= 4; i++) {
+      var item = document.createElement('li');
+      document.getElementById('board').appendChild(item);
+      document.querySelectorAll('ul#board li')[i].style.width = `${wide}px`;
+      document.querySelectorAll('ul#board li')[i].style.height = `${wide}px`;
+      wide -= 80;
+    }
+
+    document.querySelectorAll('ul#board li:nth-child(odd)').forEach(li => li.classList.add('red'));
+    document.querySelectorAll('ul#board li:nth-child(even)').forEach(li => li.classList.add('blank'));
+
+    var imgurl = "http://pngimg.com/uploads/darts/darts_PNG55.png";
+    document.querySelectorAll('ul#board li:nth-child(odd)').forEach(li => {
+      if (li.classList.contains('red')) {
+        li.innerHTML = `<img src='${imgurl}'>`;
+      }
+    });
+
+    var time = 0;
+    function afterShot() {
+      setTimeout(function () {
+        document.querySelector("#board li:nth-child(3) img").style.display = "block";
+      }, 1000);
+      setTimeout(function () {
+        document.querySelector("#board li:nth-child(5) img").style.display = "block";
+      }, 2000);
+    }
+
+    function shoot() {
+      console.log(`Time: ${time} seconds`);
+      time += 1;
+      if (time >= 2) {
+        document.querySelector("#board li:nth-child(1) img").style.display = "block";
+        afterShot();
+      }
+      if (time >= 2) {
+        clearInterval(timer);
+      }
+    }
+
+    var timer = setInterval(shoot, 1000);
+    shoot();
+
+  }, []);
+
+  return <ul id="board"></ul>;
+}
+
+export default Dartboard;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,18 @@
-// src/components/Homepage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Dartboard from './Dartboard';  // Import the Dartboard component
 
 function HomePage() {
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
+        
+        {/* Dartboard Column */}
+        <div className="col-md-6 text-center">
+          <Dartboard />
+        </div>
+        
+        {/* Description and Buttons Column */}
         <div className="col-md-6 text-center">
           <h2>Darty Party!</h2>
           <p>Choose your game mode:</p>
